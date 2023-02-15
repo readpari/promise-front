3292de52bab447f9ca8884d70765061f3882f9c5
Evgeny <e.litvinenko@mute-lab.com>
2023-02-15 11:29:44 +0400

    refactor: change storybook path

--- packages/components/src/Book/Book.stories.tsx
+++ packages/components/src/Book/Book.stories.tsx
@@ -7,7 +7,7 @@ export default {
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: "Book modal",
+  title: "Book",
   component: Book,
 };
 
