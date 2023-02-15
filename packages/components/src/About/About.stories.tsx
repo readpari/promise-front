602f7c9901a007889552f8c8c1f48a4821be3617
Evgeny <e.litvinenko@mute-lab.com>
2023-02-15 11:28:55 +0400

    added about page

---
+++ packages/components/src/About/About.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import About from "./About";
+
+export default {
+  title: "About page",
+  component: About,
+};
+
+export const Primary = () => {
+  return <About />;
+};
--- packages/components/src/About/About.tsx
+++ packages/components/src/About/About.tsx
@@ -1,7 +1,37 @@
 import * as React from "react";
+import { AppLayout } from "../index";
+import { useCallback } from "react";
 
 const About: React.FC = (props) => {
-  return null;
+  // сайдбар должен быть навигацией по сайту
+  // т.е вкладки About / Bet / Read
+  const renderSidebar = useCallback(() => {
+    return (
+      <div>
+        это контент сайдбара который см такую же функцию в
+        packages/components/src/Book/Book.tsx
+      </div>
+    );
+  }, []);
+
+  // по сути тут тебе нужно запилить отрисовку контента из bookjack
+  // с описанием что это за приложение
+  const renderContent = useCallback(() => {
+    return (
+      <div>
+        Это контент страницы см такую-же функцию в
+        packages/components/src/Book/Book.tsx
+      </div>
+    );
+  }, []);
+
+  return (
+    <AppLayout
+      title={"это заголовок страницы"}
+      renderSidebar={renderSidebar}
+      renderContent={renderContent}
+    />
+  );
 };
 
 export default About;
