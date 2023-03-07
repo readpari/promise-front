cd6631f2455c700b42bfa17b72a13dbdcc01e983
Evgeny <e.litvinenko@mute-lab.com>
2023-03-07 14:27:40 +0400

    added about view

--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { createHashRouter } from "react-router-dom";
-import { Bet, Book, Login } from "@promise-front/components";
+import { About, Bet, Book, Login } from "@promise-front/components";
 
 export default createHashRouter([
   {
     path: "/",
-    element: <Bet />,
+    element: <About />,
   },
+
   { path: "login", element: <Login /> },
+
+  { path: "bet", element: <Bet /> },
+
   {
     path: "book",
     element: <Book book={null} />,
